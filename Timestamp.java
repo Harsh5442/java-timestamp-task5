c333d57f0541b0daf139c38e67aa6733f2747465
Harsh5442 <hp010897@gmail.com>
2025-02-11 09:29:58 +0530

    Initial commit

---
+++ Timestamp.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+
+public class Timestamp {
+    public static void main(String[] args) {
+        System.out.println("Current Timestamp: " + LocalDateTime.now());
+    }
+}
