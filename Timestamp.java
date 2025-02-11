09a600dfc6e79af503d8ccafbb384d2d53bb60de
Harsh5442 <hp010897@gmail.com>
2025-02-11 10:05:44 +0530

    Updated timestamp message

--- Timestamp.java
+++ Timestamp.java
@@ -2,6 +2,6 @@ import java.time.LocalDateTime;
 
 public class Timestamp {
     public static void main(String[] args) {
-        System.out.println("Current Timestamp: " + LocalDateTime.now());
+        System.out.println("Updated Timestamp: " + LocalDateTime.now());
     }
 }
